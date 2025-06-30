e29e6cc7565595199a25008d9c1642a56539be0f
ammarshabir552299 <ammarshabir529@gmail.com>
2025-06-30 16:32:38 +0500

    updates

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,10 @@
 // src/api.ts
+
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
-import type { Schema } from "../amplify/data/resource"; // If you generated types
+import type { Schema } from "../amplify/data/resource";
 
+// ✅ This is the only thing you need:
 export const client = generateClient<Schema>({
   outputs,
 });
-
